d02e04293ed175d562f3f8e131b14daab024f031
Marcio Bigolin <marcio.bigolinn@gmail.com>
2017-08-21 11:58:07 -0300

    add User

---
+++ app/src/main/java/ifrs/canoas/model/User.java
@@ -0,0 +1,18 @@
+package com.ifrs.aluno.aula1.model;
+
+/**
+ * Created by Aluno on 06/06/2017.
+ */
+
+public class User {
+
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
