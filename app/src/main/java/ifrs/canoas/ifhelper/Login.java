1cbd27845056f709c0f9403a8b36ff4c56ca44cb
Marcio Bigolin <marcio.bigolinn@gmail.com>
2017-08-21 12:05:58 -0300

    REFACT

--- app/src/main/java/ifrs/canoas/ifhelper/Login.java
+++ app/src/main/java/ifrs/canoas/ifhelper/Login.java
@@ -2,8 +2,6 @@ package ifrs.canoas.ifhelper;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -15,7 +13,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -50,33 +47,11 @@ public class Login extends AppCompatActivity {
         tarefa.execute(uri);
     }
 
-    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
-        @Override
-        protected String doInBackground(String... urls) {
-            try {
-                return downloadUrl(urls[0]);
-            } catch (IOException e) {
-                return "Unable to retrieve web page. URL may be invalid." + e;
-            }
-        }
-
-        // onPostExecute displays the results of the AsyncTask.
-        @Override
-        protected void onPostExecute(String result) {
-            Log.d("teste", result);
-            //Gson g = new Gson();
-            //User user = g.fromJson(result.trim(), User.class);
-
-        }
-    }
-
-
     private String downloadUrl(String myurl) throws IOException {
         InputStream is = null;
-        // Only display the first 500 characters of the retrieved
-        // web page content.
+        // Limitar a 500 Caracteres lidos
         int len = 500;
-        //Log.d("DEBUG", "url: " + myurl);
+        Log.d("DEBUG", "url: " + myurl);
 
         try {
             URL url = new URL(myurl);
@@ -106,12 +81,32 @@ public class Login extends AppCompatActivity {
 
     }
 
-    public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+    public String readIt(InputStream stream, int len) throws IOException {
         Reader reader = null;
         reader = new InputStreamReader(stream, "UTF-8");
         char[] buffer = new char[len];
         reader.read(buffer);
         return new String(buffer);
+    }
+
+    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid." + e;
+            }
+        }
+
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            Log.d("teste", result);
+            //Gson g = new Gson();
+            //User user = g.fromJson(result.trim(), User.class);
+
+        }
     }
 
 
